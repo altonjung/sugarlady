e2feb555ce5ba65be5b96d539bea85d18d98f09d
alton.jung <alton.jung@gmail.com>
2025-06-21 18:31:21 +0900

    reset

--- hs2/HSPlugins-main/WorkspaceSupport.Core/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle(WorkspaceSupport.WorkspaceSupport.Name)]
-[assembly: AssemblyDescription("Keyframe sequencing/animation tool for Studio")]
-[assembly: AssemblyCompany("https://github.com/IllusionMods/HSPlugins")]
-[assembly: AssemblyProduct(WorkspaceSupport.WorkspaceSupport.Name)]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyVersion(WorkspaceSupport.WorkspaceSupport.Version)]
